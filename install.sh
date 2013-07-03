3f237aa6674f501e1568cc1d8305545580bbb34f
mdevils <mdevils@yandex.ru>
2013-07-03 21:32:14 +0400

    Fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 curl https://raw.github.com/enb-make/npm-package/master/init.sh > /tmp/npm-package-init.sh
-sh /tmp/npm-package-init.sh
+sh /tmp/npm-package-init.sh < /dev/stdin
 rm /tmp/npm-package-init.sh
