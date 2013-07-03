5fa9263a4c1915d3c19e1b3423937505d4364049
mdevils <mdevils@yandex.ru>
2013-07-03 21:28:29 +0400

    Fix

--- init.sh
+++ init.sh
@@ -1,88 +1,83 @@
 #!/bin/sh
 
-function npm_generator() {
+echo "NPM Package Generator."
 
-    echo "NPM Package Generator."
+CONFIRM="N"
 
-    CONFIRM="N"
+while true; do
 
-    while true; do
+    case "$CONFIRM" in
 
-        case "$CONFIRM" in
+        N)
+            echo
+            printf "Enter Package Name: "
+            read PackageName
 
-            N)
-                echo
-                printf "Enter Package Name: "
-                read PackageName
+            printf "Enter your First Name: "
+            read FirstName
 
-                printf "Enter your First Name: "
-                read FirstName
+            printf "Enter your Last Name: "
+            read LastName
 
-                printf "Enter your Last Name: "
-                read LastName
+            printf "Enter your Email Address: "
+            read Email
+            ;;
 
-                printf "Enter your Email Address: "
-                read Email
-                ;;
+        X)
+            exit
+            ;;
 
-            X)
-                exit
-                ;;
-
-            Y)
-                break
-                ;;
-        esac
-
-        echo
-        echo "Package Name: $PackageName"
-        echo "First Name: $FirstName"
-        echo "Last Name: $LastName"
-        echo "Email: $Email"
-        echo
-        printf "Generate npm package? [Y|N|X] "
-        read CONFIRM
-
-    done
-
-    function replace_consts {
-        while read data; do
-            echo "$data" |
-                sed 's/{{PackageNameUpperUnderscored}}/'"$PackageNameUpperUnderscored"/ |
-                sed 's/{{PackageName}}/'"$PackageName"/ |
-                sed 's/{{FirstName}}/'"$FirstName"/ |
-                sed 's/{{LastName}}/'"$LastName"/ |
-                sed 's/{{Email}}/'"$Email"/
-        done
-    }
-
-    OUTPUT_FOLDER="`pwd`/$PackageName"
-
-    if [ -r "$OUTPUT_FOLDER" ]; then
-        echo "Directory already exists: $OUTPUT_FOLDER"
-        exit 1
-    fi
-
-    PackageNameUpperUnderscored=`echo "$PackageName" | tr "[:lower:]-" "[:upper:]_"`
-
-    TMP_INPUT_FOLDER="/tmp/`date +%s`-npm-package"
-    git clone "https://github.com/enb-make/npm-package.git" "$TMP_INPUT_FOLDER"
+        Y)
+            break
+            ;;
+    esac
 
     echo
-    echo "Generating npm package..."
-    cd "$TMP_INPUT_FOLDER"
-    find . | grep -v './.git' | while read FILENAME; do
-        if [ -f "$FILENAME" ]; then
-            FILENAME=`echo "$FILENAME" | sed 's/.\///'`
-            RESULT_FILENAME=`echo "$FILENAME" | replace_consts`
-            OUTPUT_FILENAME="$OUTPUT_FOLDER/$RESULT_FILENAME"
-            mkdir -p `dirname $OUTPUT_FILENAME`
-            cat "$FILENAME" | replace_consts > "$OUTPUT_FILENAME"
-            echo "Generated $FILENAME -> $RESULT_FILENAME"
-        fi
-    done
+    echo "Package Name: $PackageName"
+    echo "First Name: $FirstName"
+    echo "Last Name: $LastName"
+    echo "Email: $Email"
+    echo
+    printf "Generate npm package? [Y|N|X] "
+    read CONFIRM
 
-    rm -Rf "$TMP_INPUT_FOLDER"
+done
+
+function replace_consts {
+    while read data; do
+        echo "$data" |
+            sed 's/{{PackageNameUpperUnderscored}}/'"$PackageNameUpperUnderscored"/ |
+            sed 's/{{PackageName}}/'"$PackageName"/ |
+            sed 's/{{FirstName}}/'"$FirstName"/ |
+            sed 's/{{LastName}}/'"$LastName"/ |
+            sed 's/{{Email}}/'"$Email"/
+    done
 }
 
-npm_generator
+OUTPUT_FOLDER="`pwd`/$PackageName"
+
+if [ -r "$OUTPUT_FOLDER" ]; then
+    echo "Directory already exists: $OUTPUT_FOLDER"
+    exit 1
+fi
+
+PackageNameUpperUnderscored=`echo "$PackageName" | tr "[:lower:]-" "[:upper:]_"`
+
+TMP_INPUT_FOLDER="/tmp/`date +%s`-npm-package"
+git clone "https://github.com/enb-make/npm-package.git" "$TMP_INPUT_FOLDER"
+
+echo
+echo "Generating npm package..."
+cd "$TMP_INPUT_FOLDER"
+find . | grep -v './.git' | while read FILENAME; do
+    if [ -f "$FILENAME" ]; then
+        FILENAME=`echo "$FILENAME" | sed 's/.\///'`
+        RESULT_FILENAME=`echo "$FILENAME" | replace_consts`
+        OUTPUT_FILENAME="$OUTPUT_FOLDER/$RESULT_FILENAME"
+        mkdir -p `dirname $OUTPUT_FILENAME`
+        cat "$FILENAME" | replace_consts > "$OUTPUT_FILENAME"
+        echo "Generated $FILENAME -> $RESULT_FILENAME"
+    fi
+done
+
+rm -Rf "$TMP_INPUT_FOLDER"
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+curl https://raw.github.com/enb-make/npm-package/master/init.sh > /tmp/npm-package-init.sh
+sh /tmp/npm-package-init.sh
+rm /tmp/npm-package-init.sh
