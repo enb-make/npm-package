6ad181bebb50f6fa54a3a25bd5ebeb2a38593f66
Marat Dulin <mdevils@yandex.ru>
2014-10-05 18:40:35 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl https://raw.github.com/enb-make/npm-package/master/init.sh > /tmp/npm-package-init.sh
+curl -L https://raw.github.com/enb-make/npm-package/master/init.sh > /tmp/npm-package-init.sh
 sh /tmp/npm-package-init.sh < /dev/tty
 rm /tmp/npm-package-init.sh
